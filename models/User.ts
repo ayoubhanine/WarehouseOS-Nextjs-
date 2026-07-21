8b0759b3ebbf77a7ee93a9df849a2bfeeeb0a62c
ayoubhanine <ayoubhanine2000@gmail.com>
2026-07-21 13:28:31 +0100

    setup project structure and configure MongoDB

---
+++ models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, models, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
