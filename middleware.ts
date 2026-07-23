dd4214329006243d75f9f1fb304ab03396d885ee
ayoubhanine <ayoubhanine2000@gmail.com>
2026-07-23 16:04:49 +0100

    dockerize next js App with docker compose

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,19 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+
+// export const config = {
+//   matcher: ["/dashboard"],
+// };
+
+
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/dashboard/:path*"],
 };
