84711908ced0d8d9e680dead2c11b8ecc8e867cc
ayoubhanine <ayoubhanine2000@gmail.com>
2026-07-24 12:52:37 +0100

    fix problem

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,11 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+
+// export const config = {
+//   matcher: ["/dashboard"],
+// };
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth;
 
 export const config = {
   matcher: ["/dashboard"],
